ee4d012d5ab1b4a33b4ead7cd294a4ce6a11ba3f
Aditya Bhandari <adityab@vmware.com>
2017-03-26 16:28:29 -0700

    Initial Commit
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/content/content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css'],
+  host: {
+    "[class.content]": "true"
+  }
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
